ddbea5246aae2ed8ac9f3bd1161b6da59cb2634b
Thanh Cong Chien <thnhcngchin@gmail.com>
2019-11-15 10:28:10 +0700

    first commit

---
+++ EX1.java
@@ -0,0 +1,34 @@
+package SEP;
+
+import java.util.Scanner;
+
+public class EX1 {
+	// nhap vao 2 so a va b bat ki sao cho a != b tinh tong tu a den b
+	public static void main(String[] args) {
+		int sum = 0;
+		int a,b;
+		
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("enter 2 numbers");
+			System.out.print("inut a = ");
+			 a = sc.nextInt();
+			System.out.print("input b = ");
+			 b = sc.nextInt();
+			System.out.print("please enter again!!!");
+			
+		}while(a==b);
+		
+		if(a<b) {
+			for(int i = a ; i <= b; i++) {
+				sum+=i;
+			}
+			System.out.println("sum of a and b is " + sum);
+		}else if(a>b) {
+			for(int i = b; i<=a;i++) {
+			}
+			System.out.println("sum of a and b is " + sum);
+		}
+		
+	}
+}
